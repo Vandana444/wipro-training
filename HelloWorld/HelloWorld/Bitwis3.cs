fda1b441f38038fc50202618a31307f75f6833df
Vandana444 <maddireddyvandana@gmail.com>
2025-07-21 18:04:25 +0530

    first commit

---
+++ HelloWorld/HelloWorld/Bitwis3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Bitwis3
+    {
+        static void Main()
+        {
+            //bitwise xor 
+            int a = 5, b = 3;
+            //101
+            //011
+            //110 result
+            Console.WriteLine(a^b);
+        }
+
+    }
+}
---
+++ HelloWorld/HelloWorld/Data.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Data
+    {
+        public void Greeting()
+        {
+            Console.WriteLine("welcome to Dotnet Training...");
+
+        }
+        internal void Company()
+        {
+            Console.WriteLine("Company is wiro..");
+
+        }
+        private void Trainer()
+        {
+            Console.WriteLine("Trainer is Prasanna...");
+        }
+    }
+}
