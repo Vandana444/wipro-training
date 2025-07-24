eef9497f05bd660242a0241ae907b7bac0676f59
Vandana444 <maddireddyvandana@gmail.com>
2025-07-24 12:52:55 +0530

    july23

---
+++ CalculationClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculationLibrary;
+
+namespace CalculationClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation calculation = new Calculation();
+            int a, b;
+            Console.WriteLine("enter 2 numbers");
+            a = Convert.ToInt32(Console.ReadLine());
+            b=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("sum is" + calculation.Sum(a, b));
+            Console.WriteLine("sub is" + calculation.Sub(a, b));
+            Console.WriteLine("sum is" + calculation.Mul(a, b));
+
+
+
+
+
+
+        }
+    }
+}
---
+++ july23/Matrix.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace july23
+{
+    internal class Matrix
+    {
+        static void Main()
+        {
+            int[,] x = new int[2, 3]
+            {
+                {1, 2, 3},
+                {5,2,8 }
+            };
+
+            for (int i = 0; i < x.GetLength(0); i++)
+            {
+                for (int j = 0; j < x.GetLength(1); j++)
+                {
+                    Console.Write(x[i, j] + "  ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
