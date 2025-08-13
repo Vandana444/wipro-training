12b6503205ef13c8828ba8e597ffbba82fcf0513
Vandana444 <maddireddyvandana@gmail.com>
2025-08-13 13:07:59 +0530

    codes

---
+++ Aug_13/BankProjectCore/BankProjectCore/Models/BankDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace BankProjectCore.Models
+{
+    public class BankDbContext : DbContext
+    {
+        public BankDbContext() { }
+
+        public BankDbContext(DbContextOptions<BankDbContext> options) : base(options)
+        {
+        }
+
+        //OnConfiguring() method is used to select and configure the data source
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>().ToTable("Account");
+            modelBuilder.Entity<Trans>().ToTable("Trans");
+
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Trans> Trans { get; set; }
+
+    }
+}
